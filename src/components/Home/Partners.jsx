bab2dd528e55547d617ee435cdeae2c4fbd2bd15
SushanGhimire <sushangmi55@gmail.com>
2021-09-11 17:22:46 +0545

    slider updated

--- src/components/Home/Partners.jsx
+++ src/components/Home/Partners.jsx
@@ -3,16 +3,16 @@ import PartnerSlider from "./PartnerSlider";
 
 function Partners() {
   return (
-    <div className={`flex flex-col  width mx-auto justify-center mt-20`}>
-      <div className="mx-auto title-text dark:text-gray-200">
+    <div className={`flex flex-col mt-20`}>
+      <div className="title-text dark:text-gray-200  width mx-auto justify-center ">
         Your key to blockchain applications
       </div>
-      <div className="text-center text-gray-700 mt-2 dark:text-gray-400">
+      <div className="text-center text-gray-700 mt-2 dark:text-gray-400 width mx-auto ">
         MetaMask provides an essential utility for blockchain newcomers, token
         traders, crypto gamers, and developers. Over a million downloads and
         counting!
       </div>
-      <div className="w-full relative  overflow-hidden">
+      <div className="w-full   overflow-hidden width mx-auto">
         <PartnerSlider />
       </div>
     </div>
