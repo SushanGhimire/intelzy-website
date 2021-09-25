ee0340f25a56f47464c2181aa76d2b2a492048d1
SushanGhimire <sushangmi55@gmail.com>
2021-09-25 15:28:52 +0545

    second service card added

--- src/components/services/ServiceCard2.jsx
+++ src/components/services/ServiceCard2.jsx
@@ -2,30 +2,35 @@ import React from "react";
 
 function ServiceCard2({ flex, title, des }) {
   return (
-    <div className={`flex justify-between items-center `}>
+    <div className={`flex justify-between items-center mt-3`}>
+      {/* left card  */}
       <div
-        className={`flex-1 border-6 border-blue-400 dark:border-gray-700 rounded-xl p-4  ${
-          flex === "row" ? "" : "opacity-0"
-        } `}
+        className={`flex-1 hidden md:block border-6 border-intelzy border-opacity-60 dark:border-gray-700 rounded-xl p-4 
+        ${flex === "right" ? "md:opacity-0" : ""}
+        `}
       >
         <div className="text-xl font-black dark:text-gray-200">{title}</div>
         <p className="text-sm dark:text-gray-400">{des}</p>
       </div>
+      {/* left line  */}
       <div
-        className={`w-10 h-2 bg-blue-400 dark:bg-gray-700 ${
-          flex === "row" ? "" : "opacity-0"
+        className={`w-10 h-1.5 bg-intelzy bg-opacity-60 border-opacity-60 dark:bg-gray-700 hidden md:block ${
+          flex === "right" ? "md:opacity-0" : ""
         }`}
       ></div>
-      <div className="rounded-full p-3 ring-8 ring-blue-400 dark:ring-gray-700"></div>
+      {/* circle  */}
+      <div className="rounded-full p-3 border-6 border-intelzy border-opacity-60 dark:ring-gray-700"></div>
+      {/* right line  */}
       <div
-        className={`w-10 h-2 bg-blue-400 dark:bg-gray-700 ${
-          flex === "rev" ? "" : "opacity-0"
+        className={`w-10 h-1.5 bg-intelzy bg-opacity-60 border-opacity-60 dark:bg-gray-700   ${
+          flex === "left" ? "md:opacity-0" : ""
         }`}
       ></div>
+      {/* right card  */}
       <div
-        className={`flex-1 border-6 border-blue-400 dark:border-gray-700 rounded-xl p-4  ${
-          flex === "rev" ? "" : "opacity-0"
-        } `}
+        className={`flex-1 border-6 border-intelzy border-opacity-60 dark:border-gray-700 rounded-xl p-4   
+        ${flex === "left" ? "md:opacity-0" : ""}
+        `}
       >
         <div className="text-xl font-black dark:text-gray-200">{title}</div>
         <p className="text-sm dark:text-gray-400">{des}</p>
