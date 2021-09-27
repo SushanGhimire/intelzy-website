de30355d4b272f269316e7501f4430b16852991b
jabir-khan <jabirkhan710@gmail.com>
2021-09-27 22:00:58 +0545

    content changed

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -12,7 +12,7 @@ function Home() {
     {
       content: "Pioneer in Blockchain, AI and Cybersecurity",
       flex: "flex-row",
-      des: "By taking on the most recent innovation advancements we are pushing the limits of how technologies can help our clients by building core blockchain solutions to provide low latency and immutability to businesses by focusing on application specific blockchain architecture development along with security audit consultancy and complete audits to evaluate cyber-related risk in the network and putting mitigating controls in place to prevent systems from attacks.We tend to use machine learning algorithms and NLP for forecasting & prediction, data management and business automation. ",
+      des: "Taking on the most recent innovation advancements we are pushing the limits of how technologies can help our clients by building core blockchain solutions to provide low latency and immutability to businesses by focusing on application specific blockchain architecture development along with security audit consultancy and complete audits to evaluate cyber-related risk in the network and putting mitigating controls in place to prevent systems from attacks.We tend to use machine learning algorithms and NLP for forecasting & prediction, data management and business automation.",
       img: front2,
       aos: "fade-left",
     },
@@ -26,7 +26,7 @@ function Home() {
     {
       content: "50+ employees with years of collaborative experience",
       flex: "flex-row",
-      des: "",
+      des: "We are a group of developers with experience and solitary concentration. We work in aiding customers assemble inventive and client confronting items at scale. We have a demonstrated history for transforming ideas into financially effective products.",
       img: front3,
       aos: "fade-left",
     },
